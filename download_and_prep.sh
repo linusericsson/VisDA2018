6bd4fb06115560889f8e901084126b116cb9c95e
Linus Ericsson <linus.ericsson@ed.ac.uk>
2022-10-12 16:27:22 +0100

    Create download_and_prep.sh

---
+++ download_and_prep.sh
@@ -0,0 +1,10 @@
+wget http://csr.bu.edu/ftp/visda/2018/detection/train.tar
+wget http://images.cocodataset.org/zips/train2017.zip
+wget http://images.cocodataset.org/zips/val2017.zip
+
+wget http://csr.bu.edu/ftp/visda/2018/detection/visda18-detection-train.txt
+wget http://csr.bu.edu/ftp/visda/2018/detection/visda18-detection-test.txt
+wget http://csr.bu.edu/ftp/visda/2018/detection/coco17-train.txt
+wget http://csr.bu.edu/ftp/visda/2018/detection/coco17-val.txt
+
+wget http://csr.bu.edu/ftp/visda/2018/detection/detection_test.tar
